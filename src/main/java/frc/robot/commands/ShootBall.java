9786d98dbb6a1a72c4036d82ed1a8f4c3825b669
Sean Bailey <srbailey@students.wcpss.net>
2022-02-25 15:28:45 -0500

    Start ShootBall

---
+++ src/main/java/frc/robot/commands/ShootBall.java
@@ -0,0 +1,9 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Shooter;
+
+public class ShootBall extends CommandBase {
+    
+    
+}
