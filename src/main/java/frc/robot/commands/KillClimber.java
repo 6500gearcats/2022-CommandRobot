bff28bdd8088d0fc7f0c86f1a6a4c6db9d4b37f7
Julia Gale <jrgale@students.wcpss.net>
2022-03-02 19:37:36 -0500

    Kill switch for Climber

---
+++ src/main/java/frc/robot/commands/KillClimber.java
@@ -0,0 +1,27 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Climber;
+
+public class KillClimber extends CommandBase {
+ 
+    private final Climber m_climberSystem;
+
+    public KillClimber(Climber theClimber) {
+        m_climberSystem = theClimber;
+        addRequirements(m_climberSystem);
+    }
+
+    @Override
+    public void initialize() {
+        m_climberSystem.stopWinch();
+        m_climberSystem.stopTilt();
+    }
+  
+    @Override
+    public boolean isFinished() {
+      return true;
+    }
+
+
+}
