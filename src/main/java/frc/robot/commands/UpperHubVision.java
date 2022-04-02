244abc5a41d7851c76666532ba7b0e8a04a304d5
Blokz101 <mquantwenty22@gmail.com>
2022-04-02 15:16:56 -0400

    Named camera and added more print infromation

--- src/main/java/frc/robot/commands/UpperHubVision.java
+++ src/main/java/frc/robot/commands/UpperHubVision.java
@@ -1,27 +1,44 @@
 package frc.robot.commands;
 import org.photonvision.*;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class UpperHubVision extends CommandBase {
 
-  PhotonCamera camera = new PhotonCamera("photonvision");
+  PhotonCamera camera = new PhotonCamera("Camera1");
 
-  public UpperHubVision() {
-  }
+  // public UpperHubVision() {
+  // }
 
-  @Override
-  public void initialize() {
-    System.out.println("BUTTON PRESSED - INIT");
-  }
+  // @Override
+  // public void initialize() {
+  //   System.out.println("BUTTON PRESSED - INIT");
+  // }
 
   @Override
   public void execute() {
+
+    //Get latest results from camera
     var result = camera.getLatestResult();
+
+    //If there are targets define best target and run vision code
     if(result.hasTargets()) {
+
+      PhotonTrackedTarget bestTarget = result.getBestTarget();
+
       System.out.println(result.getBestTarget().toString());
+      System.out.println(
+        "Area: " + bestTarget.getArea() +
+        " | Yaw: " + bestTarget.getYaw() +
+        " | Pitch: " + bestTarget.getPitch() +
+        " | Skew: " + bestTarget.getSkew()
+      );
+
     } else {
+
       System.out.println("No targets found");
+
     }
   }
 
@@ -29,8 +46,8 @@ public class UpperHubVision extends CommandBase {
   // public boolean isFinished() {
   // }
 
-  @Override
-  public void end(boolean interrupted) {
-    System.out.println("BUTTON PRESSED - END");
-  }
+  // @Override
+  // public void end(boolean interrupted) {
+  //   System.out.println("BUTTON PRESSED - END");
+  // }
 }
