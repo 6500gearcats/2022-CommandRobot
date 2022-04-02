8d4eb53827b27fd6efc85d7804f0b42459fa3e07
Blokz101 <mquantwenty22@gmail.com>
2022-04-02 17:08:01 -0400

    Changed direction to fowards

--- src/main/java/frc/robot/commands/UpperHubVision.java
+++ src/main/java/frc/robot/commands/UpperHubVision.java
@@ -57,7 +57,7 @@ public class UpperHubVision extends CommandBase {
 
       //Look at upper hub
       m_drive.arcadeDrive(
-        0.5,
+        -0.5,
         translate(bestTarget.getYaw(), -30, 30, -1, 1)
       ); 
 
