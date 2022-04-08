b6133877fd257098f60efdae0033ceb5bde397ff
Blokz101 <45378214+Blokz101@users.noreply.github.com>
2022-04-08 11:15:04 -0400

    Step vision controller experiment (#35)
    * Initial EI code
    
    * Forgot PIController file
    
    * Implimented and tuned step controller
    
    * Added comments
    
    * Removed RPM constants change
    
    * Changed kShooterSpeedFast to match main
    
    Co-authored-by: Rob Gale <rob.gale@fujitsu.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -102,10 +102,17 @@ public final class Constants {
         public static final double cameraHeight = 38;    //Height of the camera on the robot in inches
         public static final double cameraAngle = 26.412;    //Angle of the camera on the robot in degrees
         public static final double targetDistanceFromHub = 100;    //Target distance to be from hub for upper ball shooting in inches
+        public static final double maxControllerSpeed = 0.6;
         public static final double marginForError = 2;    //The margin for error in both pitch and yaw for the shot to still be accurate
-        public static final double pLinearGain = 0.06;   //Proportional gain control for the linear movement of the robot 
-        public static final double iLinearGain = 0.005;  //Integral gain control for the linear movement of the robot
-        public static final double dLinearGain = 0.002;    //Derivative gain control for the linear movement of the robot
+        public static final double[][] stepControllerArray = {    //Array of values that define what speed the robot should move at each interval 
+            {5, 0.1},
+            {15, 0.3},
+            {20, 0.4},
+            {50, 0.5},
+            {100, 0.7},
+            {150, 0.85},
+            {1000, 1}
+        };
         public static final double pAngularGain = 0.03;   //Proportional gain control for the angular movement of the robot 
         public static final double iAngularGain = 0.0001;   //Integral gain control for the angular movement of the robot
         public static final double dAngularGain = 0.0;    //Derivative gain control for the angular movement of the robot
