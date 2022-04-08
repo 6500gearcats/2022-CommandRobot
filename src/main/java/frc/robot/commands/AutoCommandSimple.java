f3a13e0e20feffe3715a622e52c45a77eb11cac6
Julia Gale <jrgale7631@gmail.com>
2022-04-07 20:43:03 -0400

    added autonomous options to smartdashboard

---
+++ src/main/java/frc/robot/commands/AutoCommandSimple.java
@@ -0,0 +1,27 @@
+package frc.robot.commands;
+import frc.robot.commands.climb.individual.ParkArm;
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Shooter;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+//TODO Make sure the code works.
+//Seth is not responsible for any damage done by this code.
+
+public class AutoCommandSimple extends SequentialCommandGroup {
+
+    public AutoCommandSimple(DriveTrain DriveTrain, Shooter shooter, Elevator elevator, Climber climber) {
+        addCommands(
+            new StoreArm(climber).withTimeout(0.2),
+            new ParkArm(climber),
+            new ShootBallFast(shooter, elevator).withTimeout(2),
+            new RunCommand(
+                    () -> DriveTrain.arcadeDrive(0.7,0), 
+                    DriveTrain
+                ).withTimeout(1)
+
+        );
+    }
+}
